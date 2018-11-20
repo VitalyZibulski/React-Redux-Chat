1cbef372a1013045eaecae1d31d4d04190ce79e8
VitalyZibulski <vitaly.zibulski@gmail.com>
2018-11-20 23:23:54 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome'
 
 class App extends Component {
   render() {
@@ -19,6 +20,8 @@ class App extends Component {
           >
             Learn React
           </a>
+            <hr/>
+          <Welcome name="World" />
         </header>
       </div>
     );
