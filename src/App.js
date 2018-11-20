28e8f6d934a9fd6b15b23a767f263111fa6c9020
VitalyZibulski <vitaly.zibulski@gmail.com>
2018-11-21 00:28:02 +0300

    initial project chat

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './Welcome';
-import List from './List';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-            <hr/>
-            <Welcome>World</Welcome>
-            <List />
-        </header>
+      <div>
+        <h1>Chat</h1>
       </div>
     );
   }
