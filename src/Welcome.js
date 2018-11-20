c62ea0ab62e6071a65a97d023e2aa72095e8e0f1
VitalyZibulski <vitaly.zibulski@gmail.com>
2018-11-20 23:38:26 +0300

    add class with destruct

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,8 +4,16 @@ import React from 'react';
 //     return <h1>Hello, {props.name}</h1>
 // }
 
-const Welcome = ({name}) => (
-    <h1>Hello, {name}</h1>
-);
+// const Welcome = ({name}) => (
+//     <h1>Hello, {name}</h1>
+// );
+
+class Welcome extends React.Component {
+    render() {
+        const {name} = this.props;
+
+        return <h1>Hello, {name}</h1>;
+    }
+}
 
 export default Welcome;
